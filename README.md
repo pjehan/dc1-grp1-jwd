776554953d954df8f1bd71b790ffd65ff55bb0f1
pjehan <pierre.jehan@gmail.com>
2018-09-26 09:34:06 +0200

    Mot de passe en SHA1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dc1-grp1-jwd
+
+Faire un forward engineer
+
+```shell
+cp config/parameters.php.dist config/parameters.php
+cd admin
+npm install
+```
--- model/entities/utilisateur_entity.php
+++ model/entities/utilisateur_entity.php
@@ -3,7 +3,7 @@
 function getUtilisateurByEmailMotDePasse(string $email, string $mot_de_passe) {
     global $connection;
     
-    $query = "SELECT * FROM utilisateur WHERE email = :email AND mot_de_passe = :mot_de_passe";
+    $query = "SELECT * FROM utilisateur WHERE email = :email AND mot_de_passe = SHA1(:mot_de_passe)";
     
     $stmt = $connection->prepare($query);
     $stmt->bindParam(':email', $email);
